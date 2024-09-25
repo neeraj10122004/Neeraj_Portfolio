730ffe3a8e4c31cd0f1009fdfd2f266e736b1966
neeraj10122004 <neerajsaiteja@gmail.com>
2024-09-25 18:11:56 +0530

    resolved structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className=" bg-black text-white w-screen h-screen flex justify-center items-center ">
+        <div className=" text-3xl underline font-bold font-mono ">HELLO</div>
+      </div>
+    </>
+  )
+}
+
+export default App
